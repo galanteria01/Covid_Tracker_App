4a47579747168c8f922d1b6b1c8c682cee5afd5f
shoury <shanuu12e@gmail.com>
2020-12-08 22:27:19 +0530

    Added recyclerview

--- app/src/main/java/com/shanu/covidtracker/Fragments/ListFragment.kt
+++ app/src/main/java/com/shanu/covidtracker/Fragments/ListFragment.kt
@@ -3,33 +3,42 @@ package com.shanu.covidtracker.Fragments
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Adapter
 import com.shanu.covidtracker.R
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.shanu.covidtracker.data.model.MainViewModel
 import com.shanu.covidtracker.data.model.MainViewModelFactory
 import com.shanu.covidtracker.data.repository.Repository
+import com.shanu.covidtracker.ui.main.adapter.Adapter
+import kotlinx.android.synthetic.main.fragment_list.*
 import kotlin.reflect.typeOf
 
 class ListFragment:Fragment(R.layout.fragment_list){
     private lateinit var viewModel: MainViewModel
-    private lateinit var adapter: Adapter
+    var adapter: Adapter?=null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val repository= Repository()
         val viewModelFactory = MainViewModelFactory(repository)
         viewModel = ViewModelProvider(this,viewModelFactory).get(MainViewModel::class.java)
         viewModel.getPost()
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
 
         viewModel.myResponseData.observe(this, Observer { response ->
             if(response.isSuccessful) {
                 Log.d("Responsive", response.body()!!.countries[0].name.toString())
+                adapter = Adapter(response.body()!!.countries)
+                recyclerView.adapter = adapter
 
 
             }
         })
+
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/shanu/covidtracker/ui/main/adapter/Adapter.kt
+++ app/src/main/java/com/shanu/covidtracker/ui/main/adapter/Adapter.kt
@@ -7,28 +7,29 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 import com.shanu.covidtracker.R
+import com.shanu.covidtracker.data.model.CountryData
 
-//class Adapter(private val listOfCountries:List<CountryData>): RecyclerView.Adapter<Adapter.ViewHolder>() {
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-//        var view = LayoutInflater.from(parent.context).inflate(R.layout.card_ticket,parent,false)
-//        return ViewHolder(view)
-//    }
-//
-//    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-//        return holder.bind(listOfCountries[position])
-//    }
-//
-//    override fun getItemCount(): Int {
-//        return listOfCountries.size
-//    }
-//
-//    class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
-//        var countryName = itemView.findViewById<TextView>(R.id.countryName)
-//        var activeCases = itemView.findViewById<TextView>(R.id.activeCases)
-//        fun bind(country:CountryData){
-//            countryName.text = country.Countries[0].Country
-//            activeCases.text = country.Countries[0].TotalConfirmed.toString()
-//        }
-//    }
-//
-//}
+class Adapter(private val listOfCountries:List<CountryData.Country>): RecyclerView.Adapter<Adapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        var view = LayoutInflater.from(parent.context).inflate(R.layout.card_ticket,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        return holder.bind(listOfCountries[position])
+    }
+
+    override fun getItemCount(): Int {
+        return listOfCountries.size
+    }
+
+    class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+        var countryName = itemView.findViewById<TextView>(R.id.countryName)
+        var activeCases = itemView.findViewById<TextView>(R.id.activeCases)
+        fun bind(country: CountryData.Country){
+            countryName.text = country.name
+            activeCases.text = country.iso2
+        }
+    }
+
+}
